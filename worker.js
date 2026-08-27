c8e269543bce2d66792e45b68afde92812f84436
aviyansah-kly <avi.yansah@kly.id>
2026-08-27 14:22:13 +0700

    Remove Worker runtime to avoid asset routing loop

--- worker.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    const path = url.pathname === '/' ? '/index.html' : url.pathname;
-    return env.ASSETS.fetch(`https://assets.local${path}${url.search}`);
-  }
-};
